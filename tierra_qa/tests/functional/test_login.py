611b88eaad969f8abfc7d2797413874e39ad98f3
Davide Moro <dmoro@tierratelematics.com>
2016-07-13 08:39:46 +0000

    avoid loggedin_selenium renaming

--- tierra_qa/tests/functional/test_login.py
+++ tierra_qa/tests/functional/test_login.py
@@ -18,11 +18,10 @@ def test_successfull_login():
 @pytest_bdd.given(pytest_bdd.parsers.parse('When I log in as {user}'))
 def i_am_loggedin_given(user, loggedin_selenium):
     """Dummy given."""
-    return loggedin_selenium
 
 
 @pytest_bdd.then('I am logged in')
-def check_loggedin_then(i_am_loggedin_given, username):
+def check_loggedin_then(loggedin_selenium, username):
     """Dummy then."""
     assert 1
     # element = i_am_loggedin_given.find_element_by_id('li_user')
