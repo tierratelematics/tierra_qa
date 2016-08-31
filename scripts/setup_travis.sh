9d88f45d744b8987853243f9090d51b5cd80b71c
Davide Moro <davide.moro@gmail,.com>
2016-08-31 23:07:06 +0200

    add more log info

--- scripts/setup_travis.sh
+++ scripts/setup_travis.sh
@@ -9,11 +9,13 @@ echo "DEBUGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG"
 echo "${DRIVER}"
 
 if [ "${DRIVER}" = "phantomjs" ]; then
+  echo "using phantomjs"
   wget -O - https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 | tar -xj
   mv phantomjs-2.1.1-linux-x86_64/bin/phantomjs bin
 fi
 
 if [ "${DRIVER}" = "firefox" ]; then
+  echo "using firefox"
   wget -O - https://github.com/mozilla/geckodriver/releases/download/v0.10.0/geckodriver-v0.10.0-linux64.tar.gz | tar xvzf -
   mv geckodriver bin
 fi
