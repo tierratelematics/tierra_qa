a6952036616cb0d387bfa82f14e47a627f4f2c25
Davide Moro <davide.moro@gmail,.com>
2016-08-31 23:26:56 +0200

    remove unneeded echo and chrome section

--- scripts/setup_travis.sh
+++ scripts/setup_travis.sh
@@ -5,23 +5,14 @@ set -ev
 mkdir bin
 export PATH="$PATH:`pwd`/bin"
 
-echo "DEBUGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG"
-echo "${DRIVER}"
-
 if [ "${DRIVER}" = "phantomjs" ]; then
-  echo "using phantomjs"
   wget -O - https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 | tar -xj
   mv phantomjs-2.1.1-linux-x86_64/bin/phantomjs bin
   phantomjs --version
 fi
 
 if [ "${DRIVER}" = "firefox" ]; then
-  echo "using firefox"
   wget -O - https://github.com/mozilla/geckodriver/releases/download/v0.10.0/geckodriver-v0.10.0-linux64.tar.gz | tar xvzf -
   mv geckodriver bin
   geckodriver --version
-fi
-
-if [ "${DRIVER}" = "chrome" ]; then
-  sleep 1
 fi
