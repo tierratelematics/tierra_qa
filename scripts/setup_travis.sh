ebc81fc2cf04aacac100c849c772d0c0afc2c3ea
Davide Moro <davide.moro@gmail,.com>
2016-08-31 23:02:03 +0200

    add debug info on setup_travis

--- scripts/setup_travis.sh
+++ scripts/setup_travis.sh
@@ -5,6 +5,9 @@ set -ev
 mkdir bin
 export PATH="$PATH:`pwd`/bin"
 
+echo "DEBUGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG"
+echo "${DRIVER}"
+
 if [ "${DRIVER}" = "phantomjs" ]; then
   wget -O - https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 | tar -xj
   mv phantomjs-2.1.1-linux-x86_64/bin/phantomjs bin
