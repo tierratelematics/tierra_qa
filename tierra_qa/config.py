7569f94281aa0955688690c5ecdcc86f56b22bdc
Davide Moro <davide.moro@gmail.com>
2016-09-14 11:03:01 +0200

    improve comments for page objects setup

--- tierra_qa/config.py
+++ tierra_qa/config.py
@@ -1,7 +1,8 @@
 # Here you can define all your page mappings with path and optionally a
-# class_page if you want your specific page object implementation
+# class_page if you want your specific page object implementation.
+# By default the tierra_qa.pages.BasePage will be instanciated
 PAGE_MAPPINGS = {
     'HomePage': {'path': '/'},
     'HelloPage': {'path': '/hello'},
-    # 'AnotherPage': {'path': '/hello', 'class_page': AnotherPage},
+    # 'AnotherPage': {'path': '/anotherpage', 'class_page': AnotherPage},
     }
