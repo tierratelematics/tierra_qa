a2b939f265637eaa44990664319511bebab21e9c
Davide Moro <davide.moro@gmail.com>
2016-09-28 13:01:45 +0200

    various fixes (better bdd semantic, put common steps to be shared)

--- tierra_qa/tests/functional/test_login.py
+++ tierra_qa/tests/functional/test_login.py
@@ -11,16 +11,3 @@ scenario = partial(pytest_bdd.scenario, "functional/login.feature")
 @scenario("Successful login")
 def test_successfull_login():
     """Login."""
-
-
-@pytest_bdd.given(pytest_bdd.parsers.parse('When I log in as {user_id}'))
-def i_am_loggedin_given(user_id, page):
-    """Logged in fixture"""
-
-
-@pytest_bdd.then('I am logged in')
-def check_loggedin_then(page, username):
-    """Assert user is logged in. Implement here your
-       project related login logics.
-    """
-    assert 1
--- tierra_qa/tests/functional/test_subpage.py
+++ tierra_qa/tests/functional/test_subpage.py
@@ -10,21 +10,3 @@ scenario = partial(pytest_bdd.scenario, "functional/subpage.feature")
 @scenario("Visit subpage")
 def test_visit_subpage():
     """Login."""
-
-
-@pytest_bdd.given(pytest_bdd.parsers.parse('I visit the {page_id} page'))
-def visit_example_page(page, page_id):
-    """Dummy given."""
-
-
-@pytest_bdd.then(pytest_bdd.parsers.parse('I am on the {page_id} page'))
-def check_example_page_url(page, base_url, page_id, page_mappings):
-    """ Check page url is corret """
-    try:
-        from urlparse import urljoin
-    except ImportError:
-        # python3 compatibility
-        from urllib.parse import urljoin
-
-    assert page.current_url == urljoin(base_url,
-                                       page_mappings[page_id]['path'])
